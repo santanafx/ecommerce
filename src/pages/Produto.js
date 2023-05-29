eb5c772dd256daad37ae738840b35f75878697fb
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-29 18:20:48 -0300

    Atualizacões de estética em Produto.js

--- src/pages/Produto.js
+++ src/pages/Produto.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './Produto.css'
 import { useParams } from 'react-router-dom'
 import { Context } from '../context/globalContext'
+import cart from '../images/icon-cart.svg'
 
 export const Produto = () => {
 
     const { dataBase } = React.useContext(Context);
     const [produtoSelecionado, setProdutoSelecionado] = React.useState('');
     const [selectImg, setSelectImg] = React.useState('');
+    const [counter, setCounter] = React.useState(0);
     const params = useParams();
 
     React.useEffect(() => {
@@ -23,23 +25,44 @@ export const Produto = () => {
         setSelectImg(event);
     }
 
+    const handleCounterAdd = () => {
+        setCounter(counter + 1);
+    }
+
+    const handleCounterSub = () => {
+        setCounter(counter - 1);
+    }
+
     return (
         <section className='produtoContainerBg'>
             <div className='produtoContainer'>
                 {console.log(produtoSelecionado)}
                 <div className='produtoImgs'>
-                    <div>
-                        <img src={selectImg} alt="" />
+                    <div className='produtoImgsMain'>
+                        <img src={selectImg} alt="Imagem do produto" />
                     </div>
-                    <div>
-                        <img onClick={(event) => { handleChangeImg(event.target.getAttribute("picture")) }} picture={produtoSelecionado.img1} src={produtoSelecionado.imgTn1} alt="" />
-                        <img onClick={(event) => { handleChangeImg(event.target.getAttribute("picture")) }} picture={produtoSelecionado.img2} src={produtoSelecionado.imgTn2} alt="" />
-                        <img onClick={(event) => { handleChangeImg(event.target.getAttribute("picture")) }} picture={produtoSelecionado.img3} src={produtoSelecionado.imgTn3} alt="" />
-                        <img onClick={(event) => { handleChangeImg(event.target.getAttribute("picture")) }} picture={produtoSelecionado.img4} src={produtoSelecionado.imgTn4} alt="" />
+                    <div className='produtoImgsTn'>
+                        <img onClick={(event) => { handleChangeImg(event.target.getAttribute("picture")) }} picture={produtoSelecionado.img1} src={produtoSelecionado.imgTn1} alt="Imagem thumbnail do produto" />
+                        <img onClick={(event) => { handleChangeImg(event.target.getAttribute("picture")) }} picture={produtoSelecionado.img2} src={produtoSelecionado.imgTn2} alt="Imagem thumbnail do produto" />
+                        <img onClick={(event) => { handleChangeImg(event.target.getAttribute("picture")) }} picture={produtoSelecionado.img3} src={produtoSelecionado.imgTn3} alt="Imagem thumbnail do produto" />
+                        <img onClick={(event) => { handleChangeImg(event.target.getAttribute("picture")) }} picture={produtoSelecionado.img4} src={produtoSelecionado.imgTn4} alt="Imagem thumbnail do produto" />
                     </div>
                 </div>
                 <div className='produtoDescricao'>
-                    bloco2
+                    <h3>SNEAKER COMPANY</h3>
+                    <h1>Fall Limited Edition Sneakers</h1>
+                    <span>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstang everything the weather can offer.</span>
+                    <div>
+                        <h2>$125.00</h2>
+                        <span>50%</span>
+                    </div>
+                    <h4>$250.00</h4>
+                    <div>
+                        <button onClick={() => handleCounterSub()}>-</button>
+                        <input type="text" disabled value={counter} />
+                        <button onClick={() => handleCounterAdd()}>+</button>
+                    </div>
+                    <button><img src={cart} /> Add to cart</button>
                 </div>
             </div>
         </section>
