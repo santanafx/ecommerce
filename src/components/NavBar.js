7d1e02a676e1e9dca0557866a1a0ffb8020bcd9c
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-29 15:20:56 -0300

    Novo componente NavBar
    Criado o menu do site de ecommerce

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './NavBar.css'
+import { Link } from 'react-router-dom'
+import logo from '../images/logo.svg'
+import cart from '../images/icon-cart.svg'
+import avatar from '../images/image-avatar.png'
+
+export const NavBar = () => {
+
+    const handleClickCart = () => {
+        console.log('Clicou carrinho')
+    }
+
+    const handleClickProfile = () => {
+        console.log('Clicou profile')
+    }
+    return (
+        <section className='navBarContainerBg'>
+            <nav className='navBarContainer'>
+                <div className="navBarLogo">
+                    <img src={logo} alt="Logo do site" />
+                </div>
+                <div className='navBarMenu'>
+                    <div><Link to='/'>Collections</Link></div>
+                    <div><Link to='/'>Men</Link></div>
+                    <div><Link to='/'>Women</Link></div>
+                    <div><Link to='/'>About</Link></div>
+                    <div><Link to='/'>Contact</Link></div>
+                </div>
+                <div className='navBarUsuario'>
+                    <img onClick={() => { handleClickCart() }} src={cart} alt="Carrinho de compras" />
+                    <img onClick={() => { handleClickProfile() }} src={avatar} alt="Avatar" />
+                </div>
+            </nav>
+        </section>
+    )
+}
---
+++ src/pages/Inicio.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Inicio = () => {
+    return (
+        <div>Inicio</div>
+    )
+}
