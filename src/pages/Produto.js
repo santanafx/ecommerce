299b637798f85b8e6831c51bf7382cd53913bd16
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-29 16:58:22 -0300

    Atualização de código Produto.js
    Foi feito a lógica onde, quando a página Produto.js é iniciada, é utilizado o hook de useEffect para carregar o produto selecionado na página inicial, carregar a imagem do produto inicial e quando o usuário clica nas imagens de thumbnail, a imagem maior do produto é trocada dinâmicamente.

--- src/pages/Produto.js
+++ src/pages/Produto.js
@@ -7,27 +7,38 @@ export const Produto = () => {
 
     const { dataBase } = React.useContext(Context);
     const [produtoSelecionado, setProdutoSelecionado] = React.useState('');
+    const [selectImg, setSelectImg] = React.useState('');
     const params = useParams();
 
     React.useEffect(() => {
         dataBase.forEach((element) => {
             if (element.id === params.id) {
-
                 setProdutoSelecionado(element);
-
+                setSelectImg(element.img1)
             }
         })
     }, [])
 
+    const handleChangeImg = (event) => {
+        setSelectImg(event);
+    }
+
     return (
         <section className='produtoContainerBg'>
             <div className='produtoContainer'>
-                <div>
-                    {console.log(produtoSelecionado)}
-                    <h1>Produto: {params.id}</h1>
-                    <img src={produtoSelecionado.img1} alt="" />
+                {console.log(produtoSelecionado)}
+                <div className='produtoImgs'>
+                    <div>
+                        <img src={selectImg} alt="" />
+                    </div>
+                    <div>
+                        <img onClick={(event) => { handleChangeImg(event.target.getAttribute("picture")) }} picture={produtoSelecionado.img1} src={produtoSelecionado.imgTn1} alt="" />
+                        <img onClick={(event) => { handleChangeImg(event.target.getAttribute("picture")) }} picture={produtoSelecionado.img2} src={produtoSelecionado.imgTn2} alt="" />
+                        <img onClick={(event) => { handleChangeImg(event.target.getAttribute("picture")) }} picture={produtoSelecionado.img3} src={produtoSelecionado.imgTn3} alt="" />
+                        <img onClick={(event) => { handleChangeImg(event.target.getAttribute("picture")) }} picture={produtoSelecionado.img4} src={produtoSelecionado.imgTn4} alt="" />
+                    </div>
                 </div>
-                <div>
+                <div className='produtoDescricao'>
                     bloco2
                 </div>
             </div>
